8910b0f130cc111249aa8142459c962f6bb4a382
95aakash <aakash95aug@gmail.com>
2019-12-10 12:42:14 +0530

    Update config.php

--- config.php
+++ config.php
@@ -6,8 +6,8 @@ require_once('google-api2/src/Google/autoload.php');
 $client = new Google_Client();
 // $client->setAuthConfig('./creds.json');
 // echo 'client set';
-$client->setClientId('64436141311-95ltn246r19d1d85nhpae7di55frd1rd.apps.googleusercontent.com');
-$client->setClientSecret('Z69Iak1fotx2vCZDKDuQsMaD');
+$client->setClientId(''); // add client add  here
+$client->setClientSecret('');
 $currentCookieParams = session_get_cookie_params();
 
 // $rootDomain = '.example.com';
